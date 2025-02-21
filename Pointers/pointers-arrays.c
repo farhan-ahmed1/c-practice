945d923a958d526ffa6b53c786b9b71d1ddac673
Farhan Ahmed <fahmed@iastate.edu>
2025-02-20 18:08:53 -0600

    more updates

--- Pointers/pointers-arrays.c
+++ Pointers/pointers-arrays.c
@@ -7,5 +7,19 @@ int main()
     printf("%d\n", &A[0]); // same thing as above. memory address of A at base or [0]
     printf("%d\n", A[0]); // value of A at index 0 
     printf("%d\n", *A); // same thing as above, dereferencing to find the value at index 0 or base zero
+    int i;
+
+    int  *p = A; // valid
+    // A++; // invalid - compilation error
+    p++; // valid
+    
+    for(i = 0; i < 5; i++) 
+    {
+        printf("Address = %d\n", &A[i]); // gives us the memory address of A at index i
+        printf("Address = %d\n", A+i); // same- gives us the memory address of A at index i
+        printf("Address = %d\n", A[i]); // value of A at index i
+        printf("Address = %d\n", *(A+i)); // same - value of A at index i via deferencing
+
+    }
 }
 
