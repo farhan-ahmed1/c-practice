18e4e66829fa8a1c7dbe822c5beccbfacc0834f2
Farhan Ahmed <fahmed@iastate.edu>
2025-02-22 20:28:35 -0600

    character arrays and pointers exercise

---
+++ arrays/character-array-pointers.c
@@ -0,0 +1,31 @@
+// Character arrays and pointers
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char C[4];
+    C[0] = 'J';
+    C[1] = 'O';
+    C[2] = 'H';
+    C[3] = 'N';
+    printf("%s\n", C); // output: JOHN`4�l% 
+    
+    // how to fix
+    char character[20]; // even if there 20 char, will only print until the null char is found
+    character[0] = 'J';
+    character[1] = 'O';
+    character[2] = 'H';
+    character[3] = 'N';
+    character[4] = '\0';
+    printf("%s\n", character); // output: prints JOHN as expected
+
+    int len = strlen(character); // string methods also depend on the null terminator
+    printf("Length = %d \n", len); // prints 4 because it stops counting after finding the null terminator. - all string methods do this
+
+    // can  also be done like this 
+    char character1[20] = "JOHN"; // valid
+    char character2[] = "JOHN"; // also valid - size doesn't need to be specified
+    char character3[5] = {'J', 'O', 'H', 'N','\0'}; // also valid but null char must be explict
+    int len1 = strlen(character1);
+    printf("%s\n", character1);
+}
