89f4467348e66ae27f75699bf35548d134b2ef48
Farhan Ahmed <fahmed@iastate.edu>
2025-02-25 17:08:34 -0600

    linked list practice

---
+++ linked-list/insert-at-begining.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* head; // global variable, can be accessed anywhere
+
+int main()
+{
+    head == NULL; // empty list
+    printf("How many numbers? \n");
+    int n, i, x;
+    scanf("%d", &n);
+
+}
